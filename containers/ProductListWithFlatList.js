4f08d35e81da586022862de0c6243007cc11c1c2
Bala Krishna Ragala <baluragala@gmail.com>
2018-05-09 23:23:17 +0530

    TODO sections add in WIP

--- containers/ProductListWithFlatList.js
+++ containers/ProductListWithFlatList.js
@@ -2,14 +2,14 @@ import React, { Component } from "react";
 import ProductListItem from "../components/ProductListItem";
 import {
   ActivityIndicator,
-  ScrollView,
   FlatList,
   RefreshControl,
-  Alert
+  Alert,
+  View
 } from "react-native";
 
-import {connect} from "react-redux"
-import {bindActionCreators} from "redux";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 import * as productActionCreators from "../actionCreators/product";
 let URI = "http://192.168.1.101:4000";
 class ProductListWithFlatList extends Component {
@@ -18,28 +18,26 @@ class ProductListWithFlatList extends Component {
   }
 
   componentDidMount() {
-    this.props.actions.getProducts(this.props.page,this.props.limit);
+    this.props.actions.getProducts(this.props.page, this.props.limit);
   }
 
   onWishTapped = id => {
-    //Alert.alert(`Product tapped :${id}`);
-    // let updateIndex = this.state.products.findIndex(p => p.id === id);
-    // this.state.products[updateIndex].wish = !this.state.products[updateIndex]
-    //   .wish;
-    // this.setState({ products: [...this.state.products] }, function() {
-    //   console.log(this.state);
-    // });
+    // TODO: when user taps on the heart icon, you 
+    // need to change the icon to full heart, which is 
+    // already handled in ProductListItem based on wish property
+    // you need to set the wish property to true for the tapped product
+    // which is already in the state
+    // implement above using react redux
   };
 
-  _getProducts = (page=1,limit=8) => {
-    this.props.actions.getProducts(page,limit);
+  _getProducts = (page = 1, limit = 8) => {
+    this.props.actions.getProducts(page, limit);
   };
 
   /*  flat list supporting methods */
 
   _getMore = () => {
-    console.log('getmore')
-    this._getProducts(++this.props.page,this.props.limit);
+    this._getProducts(++this.props.page, this.props.limit);
   };
 
   _renderItem = ({ index, item }) => {
@@ -48,7 +46,7 @@ class ProductListWithFlatList extends Component {
         {...this.props}
         id={item.id}
         title={`${item.id} - ${item.title}`}
-        image={item.image ? `${URI}/images/${item.image}`:null}
+        image={item.image ? `${URI}/images/${item.image}` : null}
         rating={item.rating}
         price={item.price}
         wish={item.wish || false}
@@ -81,38 +79,41 @@ class ProductListWithFlatList extends Component {
   /*  flat list supporting methods - END */
 
   render() {
-    return this.props.isLoading ? (
-      <ActivityIndicator size="large" color="#00ff80" />
-    ) : (
-      <FlatList
-        data={this.props.products}
-        renderItem={this._renderItem}
-        keyExtractor={this._keyExtractor}
-        onEndReachedThreshold={0.5}
-        onEndReached={this._getMore}
-        refreshControl={this._renderRefreshControl()}
-      />
+    return (
+      <View style={{flex:1,backgroundColor:'#fff'}}>
+        {this.props.isLoading ? (
+          <ActivityIndicator size="large" color="#00ff80" />
+        ) : (
+          <FlatList
+            data={this.props.products}
+            renderItem={this._renderItem}
+            keyExtractor={this._keyExtractor}
+            onEndReachedThreshold={0.5}
+            onEndReached={this._getMore}
+            refreshControl={this._renderRefreshControl()}
+          />
+        )}
+      </View>
     );
   }
-
-
 }
 
-function mapStateToProps(state){
- 
-  return{
-    products:state.productState.products,
-    isLoading:state.productState.isLoading,
+function mapStateToProps(state) {
+  return {
+    products: state.productState.products,
+    isLoading: state.productState.isLoading,
     isRefreshing: state.productState.isRefreshing,
-    page:state.productState.page,
-    limit:state.productState.limit,
-  }
+    page: state.productState.page,
+    limit: state.productState.limit
+  };
 }
 
-function mapDispatchToProps(dispatch){ 
-  return{
-    actions:bindActionCreators(productActionCreators,dispatch)
-  }
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(productActionCreators, dispatch)
+  };
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(ProductListWithFlatList);
+export default connect(mapStateToProps, mapDispatchToProps)(
+  ProductListWithFlatList
+);
